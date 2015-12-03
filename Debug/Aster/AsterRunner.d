38e93130e66cb5c6d855187eb7e994e8ee34dfdb
Luca Dall'Olio <luca.dallolio@gmail.com>
2015-12-03 18:37:24 +0100

    cleanup generated files

--- Debug/Aster/AsterRunner.d
+++
@@ -1,9 +0,0 @@
-Aster/AsterRunner.o: ../Aster/AsterRunner.cpp ../Aster/AsterRunner.h \
- ../Aster/../Abstract/SolverInterfaces.h \
- ../Aster/../Abstract/ConfigurationParameters.h
-
-../Aster/AsterRunner.h:
-
-../Aster/../Abstract/SolverInterfaces.h:
-
-../Aster/../Abstract/ConfigurationParameters.h:
--- Debug/Systus/SystusRunner.d
+++
@@ -1,9 +0,0 @@
-Systus/SystusRunner.o: ../Systus/SystusRunner.cpp \
- ../Systus/SystusRunner.h ../Systus/../Abstract/SolverInterfaces.h \
- ../Systus/../Abstract/ConfigurationParameters.h
-
-../Systus/SystusRunner.h:
-
-../Systus/../Abstract/SolverInterfaces.h:
-
-../Systus/../Abstract/ConfigurationParameters.h:
