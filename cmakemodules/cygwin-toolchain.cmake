26650bed27dc8700bdf5c8982e7eb95a2550160a
Thomas Abballe <ThomasAbballe@users.noreply.github.com>
2016-03-02 15:22:22 +0100

    Added a Cygwin toolchain file you can use via the CMAKE_TOOLCHAIN_FILE flag.\nAdded the possibility to specify your MED directory via MEDFile_DIR.

---
+++ cmakemodules/cygwin-toolchain.cmake
@@ -0,0 +1,20 @@
+#
+# Copyright (C) Alneos, s.a.r.l. (contact@alneos.fr)
+# Released under the GNU General Public License
+#
+# A Default toolchain for compiling Vega under Cygwin
+# It uses the mingw-w64 compilers tools
+# Syntax:
+#   cmake -DCMAKE_TOOLCHAIN_FILE=path/to/this/file
+#
+
+# Where you have installed Cygwin
+set(CYGWIN_MOUNT "/cygdrive/c/cygwin64")
+
+# Looking for what we need
+set(CMAKE_FIND_ROOT_PATH "${CYGWIN_MOUNT}")
+set(COMPILER_PREFIX "x86_64-w64-mingw32")
+find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
+find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
+find_program(CMAKE_AR NAMES ${COMPILER_PREFIX}-ar)
+find_program(CMAKE_RANLIB NAMES ${COMPILER_PREFIX}-ranlib)
