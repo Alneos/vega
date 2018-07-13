73692874ab2663c38ed8025f9bb961fe64af7ef7
Luca Dall'Olio <luca.dallolio@gmail.com>
2018-07-13 21:06:36 +0200

    try to fix problem in gitlab

--- Test/Abstract/Utility_test.cpp
+++ Test/Abstract/Utility_test.cpp
@@ -5,7 +5,7 @@
 
 #define BOOST_TEST_MODULE utility_tests
 #include "build_properties.h"
-#include "../../Abstract/Value.h"
+#include "../../Abstract/Utility.h"
 #include <boost/test/unit_test.hpp>
 
 using namespace std;
