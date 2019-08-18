8ed0f119bbc809b844f5a5963a462282a28d9610
Luca Dall'Olio <luca.dallolio@ext.irt-systemx.fr>
2019-08-18 20:08:10 +0200

    fix copyright heading

--- Test/Optistruct/OptistructParser_test.cpp
+++ Test/Optistruct/OptistructParser_test.cpp
@@ -1,11 +1,24 @@
 /*
- * Copyright (C) Alneos, s. a r. l. (contact@alneos.fr)
- * Released under the GNU General Public License
+ * Copyright (C) IRT Systemx (luca.dallolio@ext.irt-systemx.fr)
+ * This file is part of Vega.
  *
- * NastranParser_test.cpp
+ *   Vega is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 2 of the License, or
+ *   (at your option) any later version.
  *
- *  Created on: Dec 24, 2012
- *      Author: dallolio
+ *   Vega is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Vega.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * NastranParser.cpp
+ *
+ *  Created on: Jul 5, 2018
+ *      Author: Luca Dall'Olio
  */
 
 #define BOOST_TEST_MODULE optistruct_parser_tests
