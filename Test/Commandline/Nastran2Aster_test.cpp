592bf32d483d462fdd67c124ff25fb351338aa14
Luca Dall'Olio <luca.dallolio@ext.irt-systemx.fr>
2019-09-14 00:07:55 +0200

    test rbe2 ok

--- Test/Commandline/Nastran2Aster_test.cpp
+++ Test/Commandline/Nastran2Aster_test.cpp
@@ -278,7 +278,7 @@ BOOST_AUTO_TEST_CASE( gpstress ) {
 
 BOOST_AUTO_TEST_CASE( test4a_rbe2 ) {
     // Tests NOOK
-	CommandLineUtils::nastranStudy2Aster("/irt/test4a_rbe2/test4a_rbe2.nas", false, true, 0.00001);
+	CommandLineUtils::nastranStudy2Aster("/irt/test4a_rbe2/test4a_rbe2.nas", RUN_ASTER, true, 0.00001);
 }
 
 } /* namespace test */
